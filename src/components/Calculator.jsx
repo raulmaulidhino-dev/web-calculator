0381ab26240c6eec2a2851e229cb0949a3361ead
Raul Maulidhino <raulahmadm314@gmail.com>
2025-01-04 02:56:35 +0700

    fix: replace some inefficient logics

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -8,20 +8,20 @@ const Calculator = () => {
         { label: "8", type: "number" },
         { label: "9", type: "number" },
         { label: "÷", type: "operation" },
-        { label: "%", type: "operation" },
+        { label: "%", type: "percent" },
         { label: "4", type: "number" },
         { label: "5", type: "number" },
         { label: "6", type: "number" },
         { label: "×", type: "operation" },
-        { label: "x²", type: "quadratic" },
+        { label: "^", type: "operation" },
         { label: "1", type: "number" },
         { label: "2", type: "number" },
         { label: "3", type: "number" },
         { label: "-", type: "operation" },
-        { label: "√x", type: "square-root" },
+        { label: "√", type: "square-root" },
         { label: "0", type: "zero"},
-        { label: "+/-", type: "property" },
-        { label: ".", type: "property" },
+        { label: "00", type: "double-zero" },
+        { label: ".", type: "decimal-point" },
         { label: "+", type: "operation" }
     ];
     
@@ -30,79 +30,38 @@ const Calculator = () => {
     const [isExpanding, setIsExpanding] = useState(false);
     const [isDeg, setIsDeg] = useState(true);
     const [inputValue, setInputValue] = useState("");
+    const [isDecimalPointDisabled, setIsDecimalPointDisabled] = useState(false);
 
     const handleExpand = () => {
         setIsExpanding(!isExpanding);
     }
 
+    const handleInputChange = e => {
+        setInputValue(e.target.value);
+        setIsDecimalPointDisabled(false);
+    }
+
     const handlePrimaryKeysClick = (label, type) => {
-        label = label.replaceAll("x", "");
-        setInputValue(inputValue + label);
-
-        if (type === "number") {
-            document.querySelectorAll(".number").forEach((button) => {
-                button.disabled = false;
-            }, []);
-            document.querySelector(".zero").disabled = false;
-
-            document.querySelectorAll(".operation").forEach((button) => {
-                button.disabled = false;
-            }, []);
-
-            document.querySelector(".quadratic").disabled = false;
-            document.querySelector(".square-root").disabled = false;
-
-        } else if (type === "zero") {
-            document.querySelectorAll(".number").forEach((button) => {
-                button.disabled = true;
-            }, []);
-            document.querySelector(".zero").disabled = false;
-
-            document.querySelectorAll(".operation").forEach((button) => {
-                button.disabled = false;
-            }, []);
-
-            document.querySelector(".quadratic").disabled = false;
-            document.querySelector(".square-root").disabled = false;
-
-        } else if (type === "operation") {
-            document.querySelectorAll(".number").forEach((button) => {
-                button.disabled = false;
-            }, []);
-            document.querySelector(".zero").disabled = false;
-
-            document.querySelectorAll(".operation").forEach((button) => {
-                button.disabled = true;
-            }, []);
-
-            document.querySelector(".quadratic").disabled = true;
-            document.querySelector(".square-root").disabled = false;
-
-        } else if (type === "quadratic") {
-            document.querySelectorAll(".number").forEach((button) => {
-                button.disabled = true;
-            }, []);
-            document.querySelector(".zero").disabled = true;
-
-            document.querySelectorAll(".operation").forEach((button) => {
-                button.disabled = false;
-            }, []);
-
-            document.querySelector(".quadratic").disabled = false;
-            document.querySelector(".square-root").disabled = false;
-
-        } else if (type === "square-root") {
-            document.querySelectorAll(".number").forEach((button) => {
-                button.disabled = false;
-            }, []);
-            document.querySelector(".zero").disabled = false;
-            document.querySelectorAll(".operation").forEach((button) => {
-                button.disabled = true;
-            }, []);
-
-            document.querySelector(".quadratic").disabled = true;
-            document.querySelector(".square-root").disabled = true;
-
+        let lastChar = inputValue[inputValue.length - 1];
+        if (type === "operation") {
+            if (isNaN(Number(lastChar))) {
+                if (lastChar === "√") setInputValue(inputValue);
+                else if (lastChar === "%") setInputValue(inputValue + label);
+                else setInputValue(inputValue.slice(0, -1) + label);
+            }
+            else setInputValue(inputValue + label);
+            setIsDecimalPointDisabled(false);
+        } else if (type === "percent") {
+            if (isNaN(Number(lastChar))) setInputValue(inputValue);
+            else setInputValue(inputValue + label);
+        } else if (type === "decimal-point") {
+            if (isNaN(Number(lastChar))) setInputValue(inputValue);
+            else {
+                setInputValue(inputValue + label);
+                setIsDecimalPointDisabled(true);
+            }                
+        } else {
+            setInputValue(inputValue + label);
         }
     }
 
@@ -114,8 +73,9 @@ const Calculator = () => {
             <div class="bg-white p-6 pt-0">
                 <div className="text-slate-500 text-right">45&times;45</div>
                 <input type="text" 
-                        class="w-full text-right text-3xl py-4 focus:outline-none focus:ring-0 focus:border-0"
+                        class="w-full text-right text-3xl py-4 overflow-x-auto focus:outline-none focus:ring-0 focus:border-0"
                         value={ inputValue }
+                        onChange={handleInputChange}
                 />
             </div>
             <section className="border-t-2 border-slate-200 rounded-b-xl grid grid-cols-5 buttons">
@@ -138,17 +98,17 @@ const Calculator = () => {
                         <button className={`text-sm p-2 border-2 border-l-0 border-white rounded-r-lg ${ !isDeg ? 'bg-white' : '' }`} onClick={ () => setIsDeg(false) }>RAD</button>
                     </section>
                 </section>
-                {keys.map(({ label, type, extraClass }) => {
-                    return(
+                {keys.map(({label, type}) => {
+                    return (
                         <button
                             key={label}
-                            className={`${type}${extraClass ? ` ${extraClass}` : ""}`}
+                            className={`${type}`}
                             onClick={() => handlePrimaryKeysClick(label, type) }
-                            disabled={inputValue === "" && (type === "operation" || type === "quadratic") ? true : false}
+                            disabled={type === "decimal-point" ? isDecimalPointDisabled : false}
                         >
                             {label}
                         </button>
-                    )
+                    );
                 })}
                 <button className="operation equal-btn text-white text-xl font-extrabold bg-teal-700 rounded-br-[10px] hover:bg-teal-400">=</button>
             </section>
